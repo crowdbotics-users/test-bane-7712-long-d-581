a1d272593146f322ddb852b4b56a321e433ed667
Saurav <sp.gharti@gmail.com>
2019-01-03 21:28:18 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT erincrowdboticscom_er_581.wsgi:application
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "erincrowdboticscom-er-581"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
