acc115430650b6ed27b0f1e5ef865031cff3b064
Crowdbotics <team@crowdbotics.com>
2019-08-30 22:37:12 +0000

    add package google-assistant-sdk

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'google-assistant-sdk', 'url': 'http://pypi.python.org/pypi/google-assistant-sdk/0.4.2'},
 	{'name':'boto3', 'url': 'http://pypi.python.org/pypi/boto3/1.5.15'},
 	{'name':'django-googlecalendar', 'url': 'http://pypi.python.org/pypi/django-googlecalendar/3.0.0'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
